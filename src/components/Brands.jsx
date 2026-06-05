f02a5dd3999c39414a9b44b2c413333cb2a2f88e
aspct49-dev <aspct49@gmail.com>
2026-06-05 22:52:38 +0530

    Add Duelbits to partner marquee

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -10,7 +10,8 @@ const BRANDS = [
   { name: '500 Casino', src: '/uploads/500casino.svg', h: 64, invert: false },
   { name: 'Gamdom',     src: '/uploads/gamdom.png',    h: 90, invert: false, fit: 'left' },
   { name: 'Kick',       src: '/uploads/kick.png',      h: 38, invert: false },
-  { name: 'Juice',      src: '/uploads/juice.png',     h: 44, invert: false },
+  { name: 'Juice',      src: '/uploads/juice.png',          h: 44, invert: false },
+  { name: 'Duelbits',   src: '/uploads/duelbits-logo.webp', h: 44, invert: false },
 ];
 
 function ImageLogo({ brand }) {
